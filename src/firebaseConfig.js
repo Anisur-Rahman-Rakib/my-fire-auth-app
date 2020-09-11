aa2c01e106bc71a79142a0f57c145e03d078eedd
Anisur-Rahman-Rakib <amianis1234567890@gmail.com>
2020-09-11 20:26:56 +0600

    all file add

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBLjn2__Kkm3Jr4Fr1cEtFS7c1zsqwHg24",
+    authDomain: "ema-jhon-simple-rakib.firebaseapp.com",
+    databaseURL: "https://ema-jhon-simple-rakib.firebaseio.com",
+    projectId: "ema-jhon-simple-rakib",
+    storageBucket: "ema-jhon-simple-rakib.appspot.com",
+    messagingSenderId: "689035262025",
+    appId: "1:689035262025:web:c0a90e0c0fc693628b6798"
+  };
+
+  export default firebaseConfig;
